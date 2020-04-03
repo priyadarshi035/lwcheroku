e5c0d5fcd5410e16278d086b4f67a320f464060c
AdityaRaj-GitProject <49944456+AdityaRaj-GitProject@users.noreply.github.com>
2020-04-03 14:43:40 +0530

    app.js

---
+++ ui/app/app.js
@@ -0,0 +1,10 @@
+import { LightningElement, track } from 'lwc';
+import { navigationItems, navigationElements } from './navigation';
+
+export  default class App extends LightningElement {
+
+ connectedCallback(){
+  
+ }
+ 
+}
