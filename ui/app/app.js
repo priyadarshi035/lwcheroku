1919b4d509764bdfed57987bb465529b0d3998f3
AdityaRaj-GitProject <49944456+AdityaRaj-GitProject@users.noreply.github.com>
2020-04-03 15:05:30 +0530

    Update app.js

--- ui/app/app.js
+++ ui/app/app.js
@@ -1,10 +1,25 @@
 import { LightningElement, track } from 'lwc';
-import { navigationItems, navigationElements } from './navigation';
+import getContactList from '@salesforce/apex/ContactController.getContactList';
 
 export  default class App extends LightningElement {
-
- connectedCallback(){
-  
+ 
+ @track searchKey;
+ @track contacts;
+ @track error;
+ 
+ handleSearchKeyChange(event) {
+   this.searchKey = event.target.value;
  }
+ 
+  handleSearchClick() {
+   getContactList({searchKey:'$searchKey'}).then(data =>{ 
+            alert(JSON.stringify(data))
+            this.contacts = data;
+            this.error = undefined;
+        }).catch(error =>{
+            this.error = error;
+            this.contacts = undefined;
+        });
+  }
  
 }
